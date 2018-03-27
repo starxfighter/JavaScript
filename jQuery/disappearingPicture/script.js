f003396296e8cc232d64a05a23c46d685ce51ba9
Duane Osburn <dosburn@rocketmail.com>
2018-03-27 12:27:35 -0700

    Completed Disappearing Picture task

---
+++ jQuery/disappearingPicture/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //.hide
+    $("img").click(function() {
+        $( this ).hide();
+    });
+
+    //.show
+
+    $('.show').click(function(){
+        $('img').show();
+    })
+})
